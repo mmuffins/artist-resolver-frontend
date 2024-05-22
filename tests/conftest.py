909e61973907bb4b1b4ceadddf5c8d7645183a81
mmuffins <mail@tiv73.com>
2024-05-22 21:59:31 +0200

    format file

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,5 @@
-import hashlib
 import pytest
-import httpx
-import respx
-import json
-from unittest.mock import AsyncMock, patch, MagicMock
-from TrackManager import TrackManager, MbArtistDetails, SimpleArtistDetails, TrackManager, TrackDetails
+from unittest.mock import MagicMock
 from mutagen.id3 import TIT2, TPE1, TALB, TPE2, TIT1, TOAL, TOPE, TPE3
 
 
@@ -14,16 +9,20 @@ def mock_id3_instance(mocker):
     Fixture that returns a mocked mutagen id3 instance
     """
 
-    mocked_id3 = mocker.patch('mutagen.id3.ID3', autospec=True)
+    mocked_id3 = mocker.patch("mutagen.id3.ID3", autospec=True)
     mock_id3_instance = MagicMock()
     mocked_id3.return_value = mock_id3_instance
     # Mock the main get_id3_object call to get a dummy object
-    mocker.patch('TrackManager.TrackDetails.get_id3_object', return_value=mock_id3_instance)
+    mocker.patch(
+        "TrackManager.TrackDetails.get_id3_object", return_value=mock_id3_instance
+    )
     return mock_id3_instance
+
 
 class MockID3Tag:
     def __init__(self, text):
         self.text = text
+
 
 @pytest.fixture
 def mock_id3_tags(mock_id3_instance):
@@ -39,8 +38,8 @@ def mock_id3_tags(mock_id3_instance):
                 return value
             else:
                 return MockID3Tag(value)
-        
+
         mock_id3_instance.get.side_effect = id3_get_side_effect
         return mock_id3_instance
-    
+
     return apply_mock
