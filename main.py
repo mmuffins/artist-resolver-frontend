2f83a486e9cdd2e6074199fdb4ee80556fa8610c
mmuffins <mail@tiv73.com>
2024-05-30 21:42:42 +0200

    Make row formatting conditional

--- main.py
+++ main.py
@@ -48,45 +48,78 @@ class ArtistDelegate(QStyledItemDelegate):
                 return i
         return -1
 
+    def apply_custom_name_edited_condition(self, artist, option):
+        if not artist.custom_name_edited:
+            # set to red if the artist was not edited
+            option.palette.setColor(QPalette.ColorRole.Text, QColor(255, 23, 62))
+            return True
+        return False
+
+    def apply_server_data_condition(self, artist, option):
+        if not artist.has_server_data and isinstance(artist, MbArtistDetails):
+            # set to blue if artist was not updated from server but has mbartist details
+            option.palette.setColor(QPalette.ColorRole.Text, QColor(0, 128, 255))
+            return True
+        elif artist.has_server_data:
+            # set to purple if artist was updated from server
+            option.palette.setColor(QPalette.ColorRole.Text, QColor(164, 97, 240))
+            return True
+        return False
+
+    def apply_custom_name_edited_true_condition(self, artist, option):
+        if artist.custom_name_edited:
+            # set to green if the artist was edited
+            option.palette.setColor(QPalette.ColorRole.Text, QColor(58, 235, 157))
+            return True
+        return False
+
+    def apply_include_condition(self, artist, option, color_modified):
+        if not artist.include:
+            if color_modified:
+                # Get the current text color
+                current_color = option.palette.color(QPalette.ColorRole.Text)
+                # Reduce the color intensity
+                reduced_color = QColor(
+                    int(current_color.red() * 0.7),
+                    int(current_color.green() * 0.7),
+                    int(current_color.blue() * 0.7),
+                )
+                option.palette.setColor(QPalette.ColorRole.Text, reduced_color)
+            else:
+                # grey out entire line if the artist is not included
+                option.palette.setColor(QPalette.ColorRole.Text, QColor(95, 95, 95))
+
     def paint(
         self, painter: QPainter, option: QStyleOptionViewItem, index: QModelIndex
     ):
         column = index.column()
+        color_modified = False
 
         if not index.parent().isValid():
             # This is a track item (parent)
             track = index.internalPointer()
-
             # bold text for track rows
-            option.font.setBold(True)
+            font = option.font
+            font.setPixelSize(int(font.pixelSize() * 1.08))
+            font.setBold(True)
+            option.font = font
         else:
             # This is an artist item
             track = index.parent().internalPointer()
             artist = track.artist_details[index.row()]
 
-            if not artist.include:
-                # grey out entire line if the artist is not included
-                option.palette.setColor(QPalette.ColorRole.Text, QColor(95, 95, 95))
+            # Apply conditions
+            if column == self.custom_name_column:
+                color_modified |= self.apply_custom_name_edited_condition(
+                    artist, option
+                )
+                color_modified |= self.apply_server_data_condition(artist, option)
+                color_modified |= self.apply_custom_name_edited_true_condition(
+                    artist, option
+                )
 
-            if column == self.custom_name_column and not artist.custom_name_edited:
-                # set to red if the artist was not edited
-                option.palette.setColor(QPalette.ColorRole.Text, QColor(255, 23, 62))
-
-            if (
-                column == self.custom_name_column
-                and not artist.has_server_data
-                and type(artist) is MbArtistDetails
-            ):
-                # set to blue if artist was not updated from server but has mbartist details
-                option.palette.setColor(QPalette.ColorRole.Text, QColor(0, 128, 255))
-
-            if column == self.custom_name_column and artist.has_server_data:
-                # set to purple if artist was updated from server
-                option.palette.setColor(QPalette.ColorRole.Text, QColor(164, 97, 240))
-
-            if column == self.custom_name_column and artist.custom_name_edited:
-                # set to green if the artist was edited
-                option.palette.setColor(QPalette.ColorRole.Text, QColor(58, 235, 157))
+            # Apply the include condition last
+            self.apply_include_condition(artist, option, color_modified)
 
         super().paint(painter, option, index)
 
