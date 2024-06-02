25a289ec357c7b2643973d77a6732cef9815f767
mmuffins <mail@tiv73.com>
2024-06-02 10:14:03 +0200

    Add simple http listener

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import asyncio
 import httpx
 import webbrowser
 from Toast import Toast, ToastType
+from aiohttp import web
 from artist_resolver.trackmanager import (
     TrackManager,
     TrackDetails,
@@ -35,6 +36,38 @@ from PyQt6.QtWidgets import (
     QStyledItemDelegate,
     QStyleOptionViewItem,
 )
+
+
+class HttpServer:
+    def __init__(
+        self, main_window, host: str, port: str, loop: asyncio.ProactorEventLoop
+    ):
+        self.main_window = main_window
+        self.host = host
+        self.port = port
+        self.loop = loop
+
+    def start_server(self):
+        webapp = web.Application()
+        webapp.add_routes([web.post("/load_files", self.handle_load_files_request)])
+        runner = web.AppRunner(webapp)
+        self.loop.run_until_complete(runner.setup())
+        site = web.TCPSite(runner, self.host, self.port)
+        self.loop.run_until_complete(site.start())
+
+    async def handle_load_files_request(self, request):
+        try:
+            data = await request.json()
+            files = [
+                file["path"] for file in data["files"] if os.path.exists(file["path"])
+            ]
+            if files:
+                self.main_window.load_files(files)
+                return web.Response(text="Files loaded successfully")
+            else:
+                return web.Response(status=400, text="No valid files found")
+        except Exception as e:
+            return web.Response(status=500, text=f"An error occurred: {str(e)}")
 
 
 class ArtistDelegate(QStyledItemDelegate):
@@ -585,6 +618,7 @@ class TrackModel(QAbstractItemModel):
 
 class MainWindow(QMainWindow):
     stylesheet = "./styles.qss"
+    server_port = 23408
 
     def __init__(self, app, api_host, api_port):
         super().__init__()
@@ -610,6 +644,8 @@ class MainWindow(QMainWindow):
         self.show()
         asyncio.ensure_future(self.check_server_health(), loop=self.loop)
 
+        self.http_server = HttpServer(self, "localhost", self.server_port, self.loop)
+        self.http_server.start_server()
         app.exec()
 
     def apply_styles(self):
